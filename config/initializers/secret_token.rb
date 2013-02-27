15be520ed880f55ad515c9179c620be145f78b77
Aurora <babyaurora@gmail.com>
2013-02-27 12:27:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldVille::Application.config.secret_token = '51ccdab618f10c10b6b1a1d5c3cfc865b94c1c14433f6ea5a3d293c89f206d30c8515d3c08352c8a34a17ef63abe76840b03d9c000b8a3fb6561648a2a4e0327'
